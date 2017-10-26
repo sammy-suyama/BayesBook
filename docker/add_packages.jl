2843d6a5169220f01031cc3171dcc7f06a1a60ee
sammy-suyama <ashsuit@gmail.com>
2017-10-27 02:10:13 +0900

    create Dockerfile to make it easy to try examples.

---
+++ docker/add_packages.jl
@@ -0,0 +1,12 @@
+
+Pkg.update()
+Pkg.add("PyPlot")
+Pkg.add("StatsFuns")
+Pkg.add("SpecialFunctions")
+Pkg.add("Distributions")
+Pkg.add("PDMats")
+Pkg.add("ProgressMeter")
+Pkg.add("DataFrames")
+Pkg.add("HDF5")
+Pkg.add("JLD")
+Pkg.add("IJulia")
